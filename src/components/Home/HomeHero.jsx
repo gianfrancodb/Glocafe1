e68ead797f96cfdaa775ba2f3d9127999772bccc
gianfrancodb <gianfrancodalfiume@gmail.com>
2023-09-07 22:52:06 +0000

    a

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Data from '../../Data';
+import { motion } from 'framer-motion';
 import {
     Container,
     HeroContainer,
@@ -16,17 +17,47 @@ const HomeHero = () => {
             exit={{ opacity: 0 }}
         >
         
-            <BodyContainer>
-                <Container width={80} leftAlign bottom={2} top={10}>
+            <BodyContainer style={{
+                    width: '80%',
+                   height:'70vh',
+                   marginTop: 'calc(10vw - 80px)',
+                   backgroundColor:'#d5bdaf',
+                   borderRadius:'20px',
+                   boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                   position: 'relative',
+                }}>
+                <Container width={80} leftAlign bottom={2} top={12}>
                     <h1>{Data.home.title}</h1>
                 </Container>
-
-                <Container leftAlign bottom={6} width={65}>
+               
+                <Container  
+                leftAlign 
+                bottom={6} 
+                width={65}
+                style={{
+                    backgroundImage: 'url("/banner2.svg")',
+                    backgroundPosition: 'bottom',
+                    // Adjust as per your requirements
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    minHeight: '200px',  // Adjust based on the size of the image or content
+                }}
+            >
                     <p>{Data.home.description}</p>
                 </Container>
-                <Container style={{ alignSelf: 'flex-end' }} bottom={-10} width={65}>
-                 <img src="/Glocafe.svg" alt="Logo" style={{ maxHeight: '20vw', marginLeft: '20vw' }} />
-                </Container>
+                <Container style={{ alignSelf: 'flex-end' }} width={65} >
+                <motion.img src="/Glocafe.svg" alt="Logo" style={{
+    maxHeight: '20vw',
+    position: 'absolute',
+    top: '-7vw',
+    right: '-10vw'
+}} 
+animate={{ opacity: 1 }}
+initial={{ opacity: 0 }}
+transition={{ duration: 1, delay: 2 }}
+exit={{ opacity: 0 }}/>
+
+    </Container>
 
                 <a href="/about">
                     <Button
